4172d7a3dddc5383e114377e299a9de11d2f0c29
EMazzaglia <emiliano.mazzaglia@rootstrap.com>
2023-05-31 02:12:35 -0300

    update

---
+++ index.js
@@ -0,0 +1,44 @@
+(async function () {
+  let cargos;
+
+  async function cargarData(url) {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        cargos = data.cargos;
+        window.cargos = data.cargos;
+      })
+      .catch((error) => console.log(`Error cargando el json ${url}`, error));
+  }
+  await cargarData("porcentajes.json");
+
+  function llenarSelect() {
+    let select = document.getElementById("cargos");
+
+    cargos.forEach((cargo, index) => {
+      const opcion = document.createElement("option");
+      opcion.text = cargo.titulo;
+      opcion.value = index;
+      select.appendChild(opcion);
+    });
+  }
+
+  llenarSelect(cargos);
+})();
+
+const suplementoSIP = 47793.24;
+const base = 26.74;
+
+function handleClick() {
+  const divPorc = document.getElementById("porcentualidad");
+  const divAjuste = document.getElementById("ajuste-prop");
+  const tituloIndex = document.getElementById("cargos").value;
+  const divAjusteLabel = document
+    .getElementById("ajuste-prop-row")
+    .removeAttribute("hidden");
+  const divPorcLabel = document
+    .getElementById("porcentualidad-row")
+    .removeAttribute("hidden");
+  divPorc.innerText = `$${suplementoSIP}`;
+  divAjuste.innerText = `$${cargos[tituloIndex].ajustePorcentual}`;
+}
